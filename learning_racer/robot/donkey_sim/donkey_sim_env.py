0278a5f0ac103de7ea76ff67bc32489ee429244a
masato-ka <jp6uzv@gmail.com>
2020-08-23 10:11:52 +0900

    fix #25 Adapto to new interface of gym_donkey.

--- learning_racer/robot/donkey_sim/donkey_sim_env.py
+++ learning_racer/robot/donkey_sim/donkey_sim_env.py
@@ -4,8 +4,8 @@ import gym_donkeycar
 
 def factory_creator(sim_path, host, port, sim_track):
     def create_simulator_agent():
-        env = gym.make(sim_track,
-                       exe_path=sim_path, host=host, port=port, frame_skip=1)
+        conf = {"exe_path": sim_path, "port": port, "host": host, "frame_skip": 1}
+        env = gym.make(sim_track, conf=conf)
         return env
 
     return create_simulator_agent
